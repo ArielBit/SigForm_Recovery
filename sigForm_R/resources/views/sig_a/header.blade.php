0abdeda4b0b508f6e786d607ff5b8b5b3ad18385
Ariel Toure <arielz360007@gmail.com>
2026-01-20 14:20:14 +0000

    Add navigation links for registration and loginp

--- sigForm_R/resources/views/sig_a/header.blade.php
+++ sigForm_R/resources/views/sig_a/header.blade.php
@@ -1,5 +1,7 @@
 
 <h3>SigForm_Recovery</h3>
 <!--Lien de Connexion et d'inscription-->
+<nav>
 <a href="{{route('s'inscrire')}}">S'Inscrire</a>
 <a href="{{route('se connecter')}}">Se Connecter</a>
+</nav>    
