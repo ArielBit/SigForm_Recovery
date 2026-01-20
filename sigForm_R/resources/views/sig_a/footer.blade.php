56cdb61b8b6a81337448d93da0e3ab1634a2cd81
Ariel Toure <arielz360007@gmail.com>
2026-01-20 10:30:01 +0000

    Add footer section to footer.blade.php

---
+++ sigForm_R/resources/views/sig_a/footer.blade.php
@@ -0,0 +1,49 @@
+<hr>
+<!--SigForm-->
+<article>
+  <h3>SigForm Recovery</h3>
+  <adress>
+  <p>Cocody Centre non loin du Lycée technique, Face à l'Agefop</p>
+    <p>Cocody, Abidjan; Cote d'Ivoire</p>
+    <p><em>Téléphone:</em> +225 27 22 55 08 01</p>
+    <p><em>Email:</em> cpntic@formation.gouv.ci</p>
+  </adress>
+</article>
+
+<!--Liens Utiles-->
+<article>
+  <h3>Liens Utiles</h3>
+  <ul>
+    <li><a href='/accueil'>Accueil</a></li>
+    <li><a a href='/accueil'>A Propos</a></li>
+    <li><a a href='/accueil'>Nos Formations</a></li>
+    <li><a a href='/accueil'>Contactez Nous</a></li>
+  </ul>
+</article>
+
+<!--Nos Formations-->
+
+<article>
+  <h3>Liens Utiles</h3>
+  <ul>
+    <li><a href='/accueil'>Sécurité Informatique</a></li>
+    <li><a a href='/accueil'>Réseaux</a></li>
+    <li><a a href='/accueil'>Microsoft Office (Ms Office)</a></li>
+    <li><a a href='/accueil'>Auto CAD</a></li>
+  </ul>
+</article>
+
+<!--Suivez Nous-->
+<article>
+  <h3>Suivez Nous</h3>
+  
+    <a href='/accueil'><img src""/></a> 
+    <a href='/accueil'><img src""/></a>
+    <a href='/accueil'><img src""/></a>
+    <a href='/accueil'><img src""/></a>
+  
+</article>
+
+<hr>
+<p>Copyright <em>FlexStart</em>. Tout droits réservés.</p>
+<p>Designé par <a href="">ArielBit😎</a></p>
