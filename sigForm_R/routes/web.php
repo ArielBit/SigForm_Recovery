ce2baf98ecee8ec0fbb80f2a4d062acc40582807
Ariel Toure <arielz360007@gmail.com>
2026-01-21 11:34:41 +0000

    Add routes for form1 submission and confirmation

--- sigForm_R/routes/web.php
+++ sigForm_R/routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SigrController;
 
 Route::get('/', [SigrController::class, 'index']);
+Route::post('/form1', [SigrController::class, 'store1'])->name('form1.store1');
+Route::get('/form1/confirmation', [SigrController::class, 'confirm')->name('sign_a.confirmation'); 
