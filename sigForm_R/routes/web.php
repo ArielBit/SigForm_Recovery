e3d4a2634c917004f38611a59df2c975991e0d09
Ariel Toure <arielz360007@gmail.com>
2026-01-22 12:36:06 +0000

    Fix route definition and add delete route for form1

--- sigForm_R/routes/web.php
+++ sigForm_R/routes/web.php
@@ -5,4 +5,5 @@ use App\Http\Controllers\SigrController;
 
 Route::get('/', [SigrController::class, 'index']);
 Route::post('/form1', [SigrController::class, 'store1'])->name('form1.store1');
-Route::get('/form1/confirmation', [SigrController::class, 'confirm')->name('sign_a.confirmation'); 
+Route::get('/form1/confirmation', [SigrController::class, 'confirm')->name('sign_a.confirmation');
+Route::delete('/form1/{id}', [SigrController::class, 'destroy')->name('sign_a.confirmation');
