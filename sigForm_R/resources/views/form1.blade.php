497a02a42898c113f5bfd1db253b1581acc62113
Ariel Toure <arielz360007@gmail.com>
2026-01-26 10:40:34 +0000

    Update form1.blade.php

--- sigForm_R/resources/views/form1.blade.php
+++ sigForm_R/resources/views/form1.blade.php
@@ -1,8 +1,10 @@
 @extends('layout.app')
 @section('title','afficher')
+@section('content')
 
 @foreach($sigr as @sigrs)
 <h2>Données</h2>
 <p>Moi:{{$sigrs->ok}}</p>
 <p>Pour:{{$sigrs->po}}</p>
 @endforeach 
+@endsection
