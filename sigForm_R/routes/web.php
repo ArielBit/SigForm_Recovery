ddb3d8ba622e6fcaf8a93d3d426c57ff0876540c
Ariel Toure <arielz360007@gmail.com>
2026-01-26 10:49:04 +0000

    Add route to display form1

--- sigForm_R/routes/web.php
+++ sigForm_R/routes/web.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\SigrController;
 Route::get('/', [SigrController::class, 'index']);
 Route::post('/form1', [SigrController::class, 'store1'])->name('form1.store1');
 Route::get('/form1/confirmation', [SigrController::class, 'confirm')->name('sign_a.confirmation');
+Route::get('/form1/affich', [SigrController::class, 'showform1']);
 Route::delete('/form1/{id}', [SigrController::class, 'destroy')->name('sign_a.confirmation');
